94cb71eede30d406456ff3834c3cd2e96da490e0
binstarr <tjdqls2s@naver.com>
2022-12-26 16:21:57 +0900

    20221216 entity 완성

--- src/main/java/com/threebee/gooslegoosle/entity/ReservationEntity.java
+++ src/main/java/com/threebee/gooslegoosle/entity/ReservationEntity.java
@@ -1,36 +1,54 @@
 package com.threebee.gooslegoosle.entity;
 
 import javax.persistence.Column;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
-public class ReviewEntity {
+public class ReservationEntity {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@NotNull
 	private int id;
+
+	@NotNull
+	@Column(nullable = false)
+	private String date;
+	
+	@NotNull
+	@Column(nullable = false)
+	private String time;
+	
+	@NotNull
+	@Column(nullable = false)
+	private int headCount;
+	
+	@Column(nullable = false, length = 13)
+	@NotNull
+	@Size(min = 12, max = 13)
+	private String phoneNumber;
 	
 	@Lob
-	@NotNull
-	private String content;
+	private String request;
 	
-	@Column(nullable = false, length = 50)
-	@NotNull
-	private String starScore;
-	
-	@ManyToOne(fetch = FetchType.EAGER)
+	@ManyToOne
 	@JoinColumn(name = "userId")
 	private UserEntity user;
 	
-	@ManyToOne(fetch = FetchType.EAGER)
+	@ManyToOne
 	@JoinColumn(name = "storeId")
 	private StoreEntity store;
+	
+	@OneToMany
+	@JoinColumn(name = "menuId")
+	private MenuEntity menu;
+	
 
 }
