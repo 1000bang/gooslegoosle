a36ea8c9eaad1676d305572d97c42142a4aee8e5
1000bang <1000_bang@naver.com>
2022-12-26 16:08:02 +0900

    12/26 login mapping

--- src/main/java/com/threebee/gooslegoosle/controller/controller.java
+++ src/main/java/com/threebee/gooslegoosle/controller/controller.java
@@ -1,5 +1,22 @@
 package com.threebee.gooslegoosle.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class controller {
 
+	
+	@GetMapping({ "", "/", "index" })
+	public String fetchIndex() {
+		 return "index";
+	}
+	
+	
+	@GetMapping("/auth/login")
+	public String fetchLogin() {
+		 return "user/login_form";
+	}
+	
+	
 }
