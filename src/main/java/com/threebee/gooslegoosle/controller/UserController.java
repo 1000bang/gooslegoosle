54ecbd011e7b1884aacce5508a561ab86a836e38
iw_br <iw_br@naver.com>
2022-12-27 10:36:22 +0900

    3

--- src/main/java/com/threebee/gooslegoosle/controller/UserController.java
+++ src/main/java/com/threebee/gooslegoosle/controller/UserController.java
@@ -1,9 +1,10 @@
 package com.threebee.gooslegoosle.controller;
 
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
