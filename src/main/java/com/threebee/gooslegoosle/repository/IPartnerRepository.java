46c4c9f9a99aa2b72adecce93b73679699f570bb
1000bang <1000_bang@naver.com>
2022-12-28 11:25:21 +0900

    12/28 admin 리젝트

--- src/main/java/com/threebee/gooslegoosle/repository/IPartnerRepository.java
+++ src/main/java/com/threebee/gooslegoosle/repository/IPartnerRepository.java
@@ -14,4 +14,13 @@ public interface IPartnerRepository extends JpaRepository<StoreEntity, Integer>{
 //			+ " VALUES (?1, ?2, ?3, ?4 ) ") 
 //	public void savePartners(String phoneNumber, String storeName, String address, int postCode);
 
+	@Query(value = " SELECT "
+			+ "    * "
+			+ " FROM "
+			+ "    storeentity "
+			+ " WHERE"
+			+ "    id = ?1", nativeQuery = true)
+	public StoreEntity findByID(int id);
+	
+	
 }
