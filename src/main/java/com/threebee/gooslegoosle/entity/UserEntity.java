21fa52fbe499647123e1579a727fc8e7aa99640d
binstarr <tjdqls2s@naver.com>
2023-01-02 14:25:32 +0900

    ?

--- src/main/java/com/threebee/gooslegoosle/entity/UserEntity.java
+++ src/main/java/com/threebee/gooslegoosle/entity/UserEntity.java
@@ -1,18 +1,14 @@
 package com.threebee.gooslegoosle.entity;
 
 import java.sql.Timestamp;
-import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -82,5 +78,7 @@ public class UserEntity {
 
 	@CreationTimestamp
 	private Timestamp createdDate;
+	
+
 
 }
